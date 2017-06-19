17f0706348901a6af9f31dffedc5dd473b40051e
Michael Zick <michaelzick@gmail.com>
2017-06-19 09:40:50 -0700

    HOME: removed static title

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -6,7 +6,6 @@ class Home extends React.Component {
 
     return (
       <div>
-        <h2>Homepage template</h2>
         <h1>{pageData.title.rendered}</h1>
 
         <div dangerouslySetInnerHTML={{__html: pageData.content.rendered}} />
